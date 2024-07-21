aa80c5a4f3ccdcee38b8f0aede32c0cfbdb6b13f
DoUH <0008959@naver.com>
2024-07-21 22:50:45 +0900

    240721- Joi 수정 / Silce 작업 막바지(2)

--- src/component/Main.tsx
+++ src/component/Main.tsx
@@ -11,7 +11,7 @@ function MainPage(): JSX.Element {
 
         const test = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
-        for (var i = 0; i < 3; i--) {
+        for (var i = 0; i < 3; i++) {
             console.log(test[i])
         }
 
